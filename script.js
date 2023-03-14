5f344546af4497fed2fac1188cec9b01c0183792
KarlosMoodios <123467020+KarlosMoodios@users.noreply.github.com>
2023-03-14 21:18:04 +0000

    added CSS, and a small amount of script
    followed the tutorial to add the CSS and then edited it to make it my own, also added in a select option ready for additional plot length functionality when searching for movies.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const apiKey = "796660e5";//const variable for apikey
+// http://www.omdbapi.com/?i=tt5093958&plot=full
+const apiKey = "796660e5";
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+let plot = document.getElementById('plot-select').selectedOptions[0].value; 
+console.log(plot);
+
+async function loadMovies(searchTerm) {
+    const URL = `http://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=${apiKey}`;
+    const res = await fetch(`${URL}`);
+    const movies = await res.json();
+    // console.log(movies.Search);
+    if (movies.Response == "True") displayMovieList(movies.Search);
+};
+
+// get matches to current text input
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim();
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+};
